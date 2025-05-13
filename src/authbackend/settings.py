7af228524d3ccac30c62516bcb8b25dceb0041a1
acharyapuru <acharyapuru@users.noreply.github.com>
2025-05-13 10:41:15 +0545

    add cors middleware

--- src/authbackend/settings.py
+++ src/authbackend/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
